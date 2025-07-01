e40991bf8eab6326b35602511ec3a86741f14fd8
Tran Quang Huy <huytqhe170798@fpt.edu.vn>
2025-07-01 17:04:01 +0700

    Add layout Login

--- app/src/main/java/com/example/music_app/LoginActivity.java
+++ app/src/main/java/com/example/music_app/LoginActivity.java
@@ -8,12 +8,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
